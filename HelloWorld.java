dc7757e52106e719c5ac32ac71ecea17e1640186
Gadiel Monteabaro <silvagadiel96@gmail.com>
2026-02-10 11:17:51 -0300

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hola Gadiel, estudiando Java ?");
+        System.out.println("Gadiel: Si, Comenzando con variables.");
+    }
+}
---
+++ variables/Variables.java
@@ -0,0 +1,20 @@
+package variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        String name = "Gadiel";
+        name = "Gadiel Monteabaro";
+        int age = 29;
+
+        // constantes
+        final String EMAIL = "gadiel@gmail.com";
+
+        // declaración varF
+        var email = 2;
+
+        System.out.println("IA: Hola " + name + ", Aprendiendo Java?");
+        System.out.println(name + ": Si, empezando Java con " + age + " años.");
+        System.out.println("IA: Perfecto! veo que no tenes registrado tu email, te gustaria hacerlo?");
+        System.out.println(name + ": si, mi email es: " + EMAIL);
+    }
+}
